1edee1b747fbeb7b6257d2e9e387c733f5799f00
rahul2360 <rahulbansal2360@gmail.com>
2017-02-28 21:58:22 +0530

    basic data type answer

--- C++/Introduction/Basic data types.cpp
+++ C++/Introduction/Basic data types.cpp
@@ -2,7 +2,13 @@
 #include <cstdio>
 using namespace std;
 
-int main() {
-    // Complete the code.
+int main()
+{
+    int a;long int b;long long int c;char d;float e;double f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << a << endl << b << endl << c << endl << d << endl ;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(3) << e << endl;
+    std::cout << std::setprecision(9) << f << endl;
     return 0;
 }

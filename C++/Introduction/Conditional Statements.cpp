246ac4f067248a9c4210d92527c645b681c02c2d
rahul2360 <rahulbansal2360@gmail.com>
2017-02-28 22:01:13 +0530

    Conditional statement answer

--- C++/Introduction/Conditional Statements.cpp
+++ C++/Introduction/Conditional Statements.cpp
@@ -22,8 +22,30 @@
 #include <unordered_map>
 using namespace std;
 int main(){
-    int n;
-    cin >> n;
+    int a;
+    cin >> a;
+    if(a==1)
+        cout << "one";
+    else if(a==2)
+        cout << "two";
+    else if(a==2)
+        cout << "two";
+    else if(a==3)
+        cout << "three";
+    else if(a==4)
+        cout << "four";
+    else if(a==5)
+        cout << "five";
+    else if(a==6)
+        cout << "six";
+    else if(a==7)
+        cout << "seven";
+    else if(a==8)
+        cout << "eight";
+    else if(a==9)
+        cout << "nine";
+    else
+        cout << "Greater than 9";
     // your code goes here
     return 0;
 }

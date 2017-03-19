8682d1f9e045b0549582bf612a8a177726b570ef
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-19 21:53:56 +0530

    class answer

--- C++/Classes/class.cpp
+++ C++/Classes/class.cpp
@@ -6,7 +6,50 @@ using namespace std;
 Enter code for class Student here.
 Read statement for specification.
 */
-
+class Student
+{
+    int age;
+    int standard;
+    string first_name;
+    string last_name;
+    public:
+        void set_age(int age1)
+            {
+            age = age1;
+        }
+         void set_standard(int standard1)
+            {
+            standard = standard1;
+        }
+    void set_first_name(string first_name1)
+            {
+            first_name = first_name1;
+        }
+    void  set_last_name(string last_name1)
+            {
+            last_name = last_name1;
+        }
+    int get_age()
+        {
+        return age;
+    }
+    int  get_standard()
+        {
+        return standard;
+    }
+    string get_first_name()
+        {
+        return first_name;
+    }
+    string get_last_name()
+        {
+        return last_name;
+    }
+    void to_string()
+        {
+         cout << age << "," << first_name << ","<< last_name << ","  << standard;
+    }
+};
 int main() {
     int age, standard;
     string first_name, last_name;
@@ -23,8 +66,7 @@ int main() {
     cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
     cout << st.get_standard() << "\n";
     cout << "\n";
-    cout << st.to_string();
+    st.to_string();
 
     return 0;
 }
-

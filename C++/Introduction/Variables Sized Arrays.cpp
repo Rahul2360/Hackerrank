504bd53e90e1cc1bebe0c0292e716d454fb5701f
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-25 21:14:20 +0530

    variable sized arrays answer

--- C++/Introduction/Variables Sized Arrays.cpp
+++ C++/Introduction/Variables Sized Arrays.cpp
@@ -1,1 +1,25 @@
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,q;
+    cin >> n >> q;
+    vector<vector <int> >v(n);
+    for(int i=0;i<n;i++){
+        int num;
+        cin >> num;
+        v[i].resize(num);
+        for(int j=0;j<num;j++)
+            cin >> v[i][j];
+    }
+    for(int i=0;i<q;i++){
+        int a,b;
+        cin >> a >> b;
+        cout << v[a][b] << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}

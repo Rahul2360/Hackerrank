11ecce48ad08e19ef0ca793d070daca881138713
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-01 17:59:07 +0530

    Vector sort answer

--- C++/STL/Vector Sort.cpp
+++ C++/STL/Vector Sort.cpp
@@ -4,9 +4,17 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int>v;
+    int num,n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >>num;
+        v.push_back(num);}
+
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++)
+        cout << v[i] << " ";
     return 0;
 }

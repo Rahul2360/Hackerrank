f0b5fd3759b3ec57667aae11971cdfebe82f93b1
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-03 23:27:52 +0530

    map stl answer

--- C++/STL/maps_stl.cpp
+++ C++/STL/maps_stl.cpp
@@ -6,9 +6,26 @@
 #include <map>
 #include <algorithm>
 using namespace std;
-
-
 int main() {
+    map<string,int>m;
+    int num;
+    cin >> num;
+    for(int i=0;i<num;i++){
+        int q;
+        string str;
+        cin >> q >> str;
+        if (q == 1){
+            int mark;
+            cin >> mark;
+            m[str] += mark;
+        }
+        else if (q == 2){
+            m[str] = 0;
+        }
+        else{
+            cout << m[str] << '\n';
+        }
+    }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
 }

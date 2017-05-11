fccfbe2f0808f80f94e51a0e7fdac8a418b734f9
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-11 19:39:53 +0530

    A very Big Sum.cpp and .c executed

--- Algorithm/Warmup/C/A Very Big Sum.c
+++ Algorithm/Warmup/C/A Very Big Sum.c
@@ -7,14 +7,14 @@
 #include <stdbool.h>
 
 int main(){
-    int n;
+    int n,arr_i,i;
     scanf("%d",&n);
     long int arr[n];
-    for(int arr_i = 0; arr_i < n; arr_i++){
+    for(arr_i = 0; arr_i < n; arr_i++){
        scanf("%ld",&arr[arr_i]);
     }
     long int sum;
-    for(int i=0;i<n;i++)
+    for(i=0;i<n;i++)
         sum+=arr[i];
     printf("%ld",sum);
     return 0;

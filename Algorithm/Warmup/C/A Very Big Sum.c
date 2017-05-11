d4eb5693149857b8004e544d1dfc53919a1d77ed
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-11 19:37:20 +0530

    A very Big Sum.c answer

--- Algorithm/Warmup/C/A Very Big Sum.c
+++ Algorithm/Warmup/C/A Very Big Sum.c
@@ -9,10 +9,13 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int arr[n];
+    long int arr[n];
     for(int arr_i = 0; arr_i < n; arr_i++){
-       scanf("%d",&arr[arr_i]);
+       scanf("%ld",&arr[arr_i]);
     }
+    long int sum;
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    printf("%ld",sum);
     return 0;
 }
-

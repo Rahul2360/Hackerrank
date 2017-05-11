2acc0ae62fa8a6fdc40008d3f47cfef80a135cef
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-11 19:39:13 +0530

    A very Big Sum.cpp answer

--- Algorithm/Warmup/C++/A Very Big Sum.cpp
+++ Algorithm/Warmup/C++/A Very Big Sum.cpp
@@ -4,15 +4,16 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-
 int main(){
     int n;
     cin >> n;
-    vector<int> arr(n);
+    vector<long int>arr(n);
     for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
     }
+    long int sum=0;
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    cout << sum;
     return 0;
 }
-

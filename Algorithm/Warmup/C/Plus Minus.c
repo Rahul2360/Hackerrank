c86c4edae1bdd34af22f9a18ed4cabda8d33340f
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-13 15:50:42 +0530

    plus minus.c answer

--- Algorithm/Warmup/C/Plus Minus.c
+++ Algorithm/Warmup/C/Plus Minus.c
@@ -13,6 +13,19 @@ int main(){
     for(int arr_i = 0; arr_i < n; arr_i++){
        scanf("%d",&arr[arr_i]);
     }
+    int positive=0,negative=0,zero=0;
+    float fpositive,fnegative,fzero;
+    for(int i=0;i<n;i++){
+        if(arr[i]>0)
+            positive++;
+        else if(arr[i]<0)
+            negative++;
+        else
+            zero++;
+    }
+    fpositive = (float)positive/n;
+    fnegative = (float)negative/n;
+    fzero = (float)zero/n;
+    printf("%f\n%f\n%f",fpositive,fnegative,fzero);
     return 0;
 }
-

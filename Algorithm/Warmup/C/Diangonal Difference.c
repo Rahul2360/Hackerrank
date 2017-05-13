74decdd97103d130b9043e67e37fc6b5cd6df4db
Rahul2360 <rahulbansal2360@gmail.com>
2017-05-13 15:49:11 +0530

    Diagonal difference.c variable changes

--- Algorithm/Warmup/C/Diangonal Difference.c
+++ Algorithm/Warmup/C/Diangonal Difference.c
@@ -7,22 +7,22 @@
 #include <stdbool.h>
 
 int main(){
-    int n;
+    int n,a_i,i,a_j,j;
     scanf("%d",&n);
     int a[n][n];
-    for(int a_i = 0; a_i < n; a_i++){
-       for(int a_j = 0; a_j < n; a_j++){
+    for(a_i = 0; a_i < n; a_i++){
+       for(a_j = 0; a_j < n; a_j++){
 
           scanf("%d",&a[a_i][a_j]);
        }
     }
     int sum1=0,sum2=0,total=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
             if(i==j)
                 sum1+=a[i][j];
     }}
-    for(int i=0;i<n;i++){
+    for(i=0;i<n;i++){
         sum2+=a[i][n-1-i];
     }
     total=abs(sum1-sum2);
